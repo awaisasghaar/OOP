c38d93ce594d47bc8f2294c9bfca098d77e09cfd
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-11 15:12:24 +0500

    Modify Inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,4 @@
+# Parent class: Person
 class Person:
     def __init__(self, name, age, cgpa, subject):
         self.name = name
@@ -8,22 +9,30 @@ class Person:
     def intro(self):
         print(f'{self.name} is {self.age}-years-old and got {self.cgpa} cgpa')
 
+# Child class: Student
 class Student(Person):
+    def __init__(self, name, age, cgpa, subject, semester):
+
+        # super function will call the parent class
+        super().__init__(name, age, cgpa, subject) 
+        self.semester = semester
+
     def study(self):
-        print(f'He is a {self.subject} student')
+        print(f'He is a {self.subject} student and currently in {self.semester} semester')
 
 if __name__ == '__main__' :
-    my_intro = Student('Awais', 22, 2.8, 'Computer sciecne')
+    my_intro = Student('\nAwais', 22, 2.8, 'Computer sciecne', '6th') # Object
 
     # Enter the input
-    a = input('Enter details: ')
+    # a = input('Enter details: ')
 
-    # Using if and else statement
-    if a in ['Awais', 22, 2.8, 'Computer science']:
-        print('\n Valid details')
-        my_intro.intro()
-        my_intro.study()
+    # # Using if and else statement
+    # if a in ['Awais', 22, 2.8, 'Computer science']:
+    #     print('\n Valid details')
 
-    else:
-        print('Invlaid Details.')
+    # else:
+    #     print('Invlaid Details)
 
+
+my_intro.intro() # Parent method
+my_intro.study() # Child method
