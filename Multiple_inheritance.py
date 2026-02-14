de6fea6b95f10c17d38b98fb82c1dd18d6590198
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-14 22:59:31 +0500

    Modify this file

--- Multiple_inheritance.py
+++ Multiple_inheritance.py
@@ -1,37 +1,41 @@
 # Parent class
 class Person():
-    def __init__(self, name, age, subject):
+    def __init__(self, name, **kwargs):
+        super().__init__(**kwargs) # It will continue MRO chain
         self.name = name
-        self.age = age
-        self.subject = subject
-    
+
     def intro(self):
-        print(f"His name is {self.name} age is {self.age} subject is {self.subject}")
+        print(f"Hey, My name is {self.name}")
 
 # Child class
-class Student(Person):
-    def __init__(self, name, age, subject, university):
-        # Self method
-        super().__init__(name, age , subject)
+class Student():
+    def __init__(self, university, **kwargs):
+        super().__init__(**kwargs)
         self.university = university
     
     def study(self):
-        print(f"{self.name} is at {self.university} studying {self.subject} and age is {self.age}")
+        print(f"My Institution is {self.university} of Pakistan")
 
-# Another child class            
-class Cgpa(Student):
-    def __init__(self, name, age, subject, university, cgpa):
-
-        # Self mtehod
-        super().__init__(name, age , subject, university)
-        self.cgpa = cgpa
+# Child class
+class Student_2():
+    def __init__(self, age, **kwargs):
+        super().__init__(**kwargs)
+        self.age = age
     
+    def study_2(self):
+        print(f"{self.age}")
+
+# Inherits from all Person, Student, Student_2          
+class info(Person, Student, Student_2):
+    def __init__(self, name, university, age):
+        super().__init__(name=name, university=university, age=age)
+
     def edu_info(self):
-        print(f"{self.name} is at {self.university} studying {self.subject} and age is {self.age} and {self.cgpa}")
+        print(f"{self.name} is at {self.university} and He is {self.age}")
 
 if __name__ == '__main__':
 #    my_intro = Person('Awais', 22, 'Computer Science', 'Virtual University')
-   my_intro = Cgpa('Awais', 22, 'Computer science', 'Virual University', 2.8)
+   my_intro = info('Awais', 'Virtual University', '22-years-old')
 
    my_intro.intro() # Parent method
    my_intro.study() # Child method
