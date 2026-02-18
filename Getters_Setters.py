040d781e5d710d39ac20645b303592e41045f943
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-18 22:22:04 +0500

    Make changes

--- Getters_Setters.py
+++ Getters_Setters.py
@@ -1,16 +1,25 @@
-class Circle:
+class Circle():
     def __init__(self, radius):
         self._radius = radius
-
+    
     @property
     def radius(self): # A getter to get the radius
         return self._radius
-  
-    @property
-    def area(self):  # A getter to calculate area
-        return 3.14 * (self._radius ** 2)
-
-my_circle = Circle(3)
-
-print(my_circle.radius) # 3
-print(my_circle.area) # 28.26
+    
+    @radius.setter
+    def radius(self, value): # A setter to set the radius
+        if value <= 0:
+            raise ValueError('Value must be positive')
+        self._radius = value
+    
+if __name__ == '__main__':
+    # val = int(input("Enter the value: "))
+    try:
+       cal_circle = Circle(5)
+       cal_circle.radius = 3
+       print('Initial radius: ', cal_circle.radius)
+       cal_circle.radius = 7
+       print('Modified radius: ', cal_circle.radius)
+       cal_circle.radius = 0
+    except ValueError as e:
+        print(f"{e}")
