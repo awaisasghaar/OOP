d094c1e611aeef64a2ff6c0169792474542836dc
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-19 21:47:10 +0500

    Refactor Getters_Setters.py

--- Getters_Setters.py
+++ Getters_Setters.py
@@ -2,24 +2,40 @@ class Circle():
     def __init__(self, radius):
         self._radius = radius
     
+    # Getter
     @property
     def radius(self): # A getter to get the radius
         return self._radius
     
+    # Setter
     @radius.setter
     def radius(self, value): # A setter to set the radius
         if value <= 0:
             raise ValueError('Value must be positive')
         self._radius = value
     
+    # Deleter
+    @radius.deleter
+    def radius(self):
+        print("Deleting radius")
+        del self._radius
+
+    
 if __name__ == '__main__':
     # val = int(input("Enter the value: "))
-    try:
+       # Create circle object with radius
        cal_circle = Circle(5)
-       cal_circle.radius = 3
-       print('Initial radius: ', cal_circle.radius)
-       cal_circle.radius = 7
-       print('Modified radius: ', cal_circle.radius)
-       cal_circle.radius = 0
-    except ValueError as e:
-        print(f"{e}")
+       print(cal_circle.radius)
+
+       # Delete the radius
+       # This calls the deleter
+       del cal_circle._radius 
+       print('Radius deleted ')
+
+    #    cal_circle.radius = 7
+    #    print('Modified radius: ', cal_circle.radius)
+    #    cal_circle.radius = 0
+       try:
+           print(cal_circle.radius)
+       except AttributeError as e:
+           print(f"OOPS! {e}")
