65a687689f283608b47df36f41da1b7cacfaab32
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-27 22:17:41 +0500

    Rewrite

--- Method_Overloading.py
+++ Method_Overloading.py
@@ -1,21 +1,27 @@
-class A:
-    def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-
-    def task(self, *args):
-        result = 1
-        for int in args:
-            result *= int
-        return result
+class Acount:
+    def __init__(self, acount_1=0, acount_2=0, acount_3=0):
+        self.acount_1 = acount_1
+        self.acount_2 = acount_2
+        self.acount_3 = acount_3
     
+    def __str__(self):
+        return f'{self.acount_1} PKR, ${self.acount_2}, {self.acount_3} Pound'
+    
+    def __add__(self, other):
+        acount_1 = self.acount_1 + other.acount_1
+        acount_2 = self.acount_2 + other.acount_2
+        acount_3 = self.acount_3 + other.acount_3
+        return Acount(acount_1, acount_2, acount_3)
+
 if __name__ == '__main__':
-   print('\nSimilar to Method overloading')
-   
-   val = A(a=2, b=3, c=4, d=5)
-   print('Result is', val.task(val.a, val.b, val.c, val.d))
+    amount_1 = Acount(20000, 250, 150)
+    print(f'\n','Acount 1 amount is ', amount_1)
+    amount_2 = Acount(30000, 350, 250)
+    print(f'Acount 2 amount is ', amount_2)
+    
+    total_amount = amount_1 + amount_2
+    print(f'The total amount is ', total_amount)
+
 
 
         
