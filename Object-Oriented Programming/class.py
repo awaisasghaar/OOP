bba27f667e79d691b077fa7967c5582362e7561c
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-26 16:13:34 +0500

    Make changes

--- Object-Oriented Programming/class.py
+++ Object-Oriented Programming/class.py
@@ -1,44 +1,25 @@
-class Program():
+class library:
     def __init__(self):
-        # self.details = {'Awais': '+92-3006941108', 'Umair': '+92-3007632514'}
         self.info = {}
         
-    # def search(self):
-    #     while True:
-    #           a = str(input('Name: '))
-    #           if a in self.details:
-    #               print(f"Name is {a} phone # is {self.details[a]}")
-    #           else:
-    #               print(f"{a} is not in dictionary")
-    #               break
-
-        # return self.details
-    
     def add_info(self):
-        # Name validation
         while True:
-            name = input("Name: ")
-            if len(name) < 3:
-                print("Add at least 3 characters.")
-            else:
+            a = input("Name: ")
+            if len(a) <= 15:
+                print(f"{a}")
                 break
-
-        # Phone validation
-        while True:
-            phone = input("Phone #: ")
-            if not phone.isdigit():
-                print("Digits only.")
-            elif len(phone) != 11:
-                print("Must be exactly 11 digits.")
             else:
-                break
+                print(f"{a} is not valid")
 
-        self.info[name] = phone
-        print(f"{name} added successfully.")
+            b = input("Phone #: ")
+            if len(b) != 11:
+                print(f"Invlaid input")
+            else:
+                print(f"{b}")
+                break
+        self.info = {"Name": a, "Phone #": b}
         return self.info
-        
+
 if __name__ == '__main__':
-    find = Program()
-    # find.search()
+    find = library()
     find.add_info()
-    print(find.add_info())
